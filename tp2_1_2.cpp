ce677fef4e7839b21b7cae8e431810e346424c07
Leonel Herrera - Lic. Informatica <leonelherreradkno@gmail.com>
2019-03-23 11:30:00 -0300

    Intento de arreglo 2

--- tp2_1_2.cpp
+++ tp2_1_2.cpp
@@ -1,22 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include<iostream>
-#define N 4 //Filas
-#define M 5 //Columnas
-//*((*mt +f ) +c)
+#include<time.h>
+
+#define N 4
+#define M 5
 void Ing_Datos_Matriz(double *punt, int f, int c);
 void Mostrar_Datos_Matriz(double *punt, int f, int c);
 int main(){
-	int f,c; //F=Filas, C=Columnas
-    double mt[N][M]; //Matriz
+	int f,c;
+    double mt[N][M];
     srand(time(NULL));
-    Ing_Datos_Matriz(&mt[0][0], f,c);
-    puts("La Matriz es: ");
-    Mostrar_Datos_Matriz(&mt[0][0], f,c);
+    
+    Ing_Datos_Matriz(&mt[0][0] , f, c);
+    Mostrar_Datos_Matriz(&mt[0][0] , f, c);
+    
+    //Datos de la matriz
     printf("\n");
-    printf("Direccion de memoria de la matriz: %p\n", mt);
-	printf("Tamanio en  bytes: %d\n", sizeof(mt));
+    printf("Direccion de la memoria de la matriz: %p\n", mt);
+    printf("Tamanio en bytes  de la matriz: %d\n", sizeof(mt));
+	
 	return 0;  
 }
 
@@ -24,22 +26,22 @@ void Ing_Datos_Matriz(double *punt, int f, int c){
 	int k=0;
 	for(f=0;f<N; f++){
 		for(c= 0;c<M; c++){
-			//mt[0][0] = 1+rand()%(9);
-			*(punt+k) = 1+rand()%(9);
+			*(punt+k) = 1 + rand()%(9);
 			k++;
 		}
 	}
 }
 void Mostrar_Datos_Matriz(double *punt, int f, int c){
 	int k=0;
-	for(f=0;f<N; f++){
-		for(c= 0;c<M; c++){
-			//printf("[ %.2lf ]", *((*mt +f ) +c));
-			printf("[ %.2lf ]", *(punt+k));
+	for(f = 0;f<N; f++){
+		for(c = 0;c<M; c++){
+			printf("[ %.1lf ]", *(punt+k));
 			k++;
 		}
 		printf("\n");
 	}
-	printf("Direccion de memoria del puntero: %p\n", punt);
-	printf("Tamanio en  bytes: %d\n", sizeof(punt));
+	printf("\n");
+    printf("Direccion de la memoria del puntero: %p\n", punt);
+    printf("Tamanio en bytes  del puntero: %d\n", sizeof(punt));
 }
+
